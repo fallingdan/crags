66f2654403a73bd7fba53c39adccbef263b359e6
Dan Wagner <onewaylimited@gmail.com>
2023-01-23 16:35:49 -0600

    init commit

---
+++ src/main/java/com/github/fallingdan/crags/CragsApplication.java
@@ -0,0 +1,13 @@
+package com.github.fallingdan.crags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CragsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CragsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fallingdan/crags/CragsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fallingdan.crags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CragsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
